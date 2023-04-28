157d7bc1b0ce6c158ce97d51c139318c6fb31c32
unknown <etimubongabasi@gmail.com>
2023-04-28 16:21:58 -0700

    -

--- ex.py
+++ ex.py
@@ -1,17 +1,10 @@
 
-n = int(input('Any number: '))
+n = int(input('A FIBONACCI number: '))
+G = 1.61803398874
 
+def NextFibonacciNum(n):
+    return round(n * G)
 
-def fibonacciNum(n):
-    if n <= 3 and n >= 0:
-        return f'{n} is a fibonacci number'
-    elif n == 5 or n == 8 or n == 13 or n == 21 or n == 34:
-        return f'{n} is a fibonacci number'
     
-    
-    while n > 34 :  
-       n == n * 1.6180
-       break
-            
-    return n
+print(NextFibonacciNum(n)) 
     
--- fibonacci.py
+++ fibonacci.py
@@ -4,8 +4,8 @@ import math
 import math
 
 def perfectSquare(x):
-    square = math.sqrt(x)
-    return square * square == x
+    square = int(math.sqrt(x))
+    return int(square * square) == x
 
 def fibNo(n,):
     return perfectSquare(5*n*n + 4) or perfectSquare(5*n*n - 4) 
@@ -13,6 +13,8 @@ def fibNo(n,):
 
 N = int(input('number: '))
 def isFib(N):
+    if N == 0 or N == 1 or N == 2 or N ==3:
+        return f' {N} is a fibonacci number'
     if fibNo(N) == True:
         return f' {N} is a fibonacci number'
     else:
@@ -21,7 +23,7 @@ def isFib(N):
 print(isFib(N))
 
 w = 1 +math.sqrt(5)
-b = 1- math.sqrt(5)
+b =  math.sqrt(5) - 1
 print(w,b)
 
        
